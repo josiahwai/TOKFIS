69d0b976431748987ed03a9f729c20abf61fec40
Josiah Wai <jwai@princeton.edu>
2023-05-01 14:51:43 -0400

    add workaround for variable-sized control outputs

--- tokamaks/nstxu/call_controlfun.m
+++ tokamaks/nstxu/call_controlfun.m
@@ -1,17 +1,21 @@
 
-function u = call_controlfun(r, rdot, y, ydot, tnow, CONFIG)
+function upad = call_controlfun(r, rdot, y, ydot, tnow, CONFIG)
 
-% convert unicode to string. (simulink has shitty string-handling
-% capabilities)
+% convert unicode to string 
 controlfun_nam = char(CONFIG.controlfun_unicode);   
 
 % create handle to controlfun
 controlfun = str2func(controlfun_nam);
 
+% 
+% u = zeros(CONFIG.nu,1);
+% coder.varsize('u', CONFIG.nu, 0);
+
 % call controlfun
 u = controlfun(r, rdot, y, ydot, tnow, CONFIG);
 
-
+upad = zeros(100,1);
+upad(1:CONFIG.nu) = u;
 
 
 
